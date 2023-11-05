747ce90d88c82ac121e14f6d1b9d2b1f36d71fb9
Wenming Pan <pwm@google.com>
2023-11-05 23:58:03 +0800

    add url.txt

--- __main__.py
+++ __main__.py
@@ -1,21 +1,17 @@
 import pulumi
 import pulumi_gcp as gcp
 
-config = pulumi.Config();
-name = config.require('name');
+config = pulumi.Config()
+name = config.require('name')
 type = config.require('type')
 
-url='https://static-gl.farlightgames.com/p/pcsdk/launcher/10060/prodef4621e968ad6b383b8754148b63/0/gamepack/498/gamepack.zip'
+download_str = ''
 
-# region_list = ['us-west1','us-west2','us-west3','us-west4','us-central1','us-east1',
-# 'us-east4','us-east5','us-south1' ,'northamerica-northeast1' ,'northamerica-northeast2' ,'southamerica-west1' ,
-# 'southamerica-east1', 'europe-west2', 'europe-west1', 'europe-west4', 'europe-west6', 'europe-west3', 'europe-north1',
-# 'europe-central2', 'europe-west8', 'europe-southwest1', 'europe-west9', 'europe-west12', 'europe-west10', 'asia-south1',
-# 'asia-south2', 'asia-southeast1', 'asia-southeast2', 'asia-east2', 'asia-east1', 'asia-northeast1', 'asia-northeast2',
-# 'australia-southeast1' ,'australia-southeast2' ,'asia-northeast3' ,'me-west1' ,'me-central1']
+with open('url.txt') as f:
+    for line in f.readlines():
+        line = line.strip()
+        download_str += "curl -i %s > /dev/null;" % (line)
 
-# #bucket = gcp.storage.Bucket(name, location="US")
-# #pulumi.export('bucket_name', bucket.url)
 
 def create_vm(name, region, type):
     instance = gcp.compute.Instance(resource_name=name+'-'+region,
@@ -35,9 +31,8 @@ def create_vm(name, region, type):
         metadata_startup_script='''
     #!/bin/bash
     for i in {1..3}; do
-    curl -i -r 0-100000000 '''+url+''' > /dev/null;
-    done
-    sudo shutdown -h now''',
+    '''+download_str+'''
+    done''',
     )
     pulumi.export('instance_name', instance.name)
 
